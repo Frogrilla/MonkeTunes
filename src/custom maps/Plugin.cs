844271d5c10132d3f21e4d11b06fd484f7859706
Frogrilla <40333513+Frogrilla@users.noreply.github.com>
2022-11-13 18:05:29 +0000

    Attempt at adding better map support
    No event in map loader for OnMapLeave so it can't be improved currently (issue raised)

--- src/custom maps/Plugin.cs
+++ src/custom maps/Plugin.cs
@@ -12,10 +12,14 @@ namespace MT_CustomMaps
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
     public class Plugin : BaseUnityPlugin
     {
+        public Vector3 lastPos;
+        public Vector3 lastRot;
+        public bool posUpdated = false;
         public void Start()
         {
             HarmonyPatches.ApplyHarmonyPatches();
             Events.OnMapEnter += OnMapEnter;
+            //Events.OnMapChange += OnMapChange;
         }
 
         // Find JukeBox and send it to position in map if it exists
@@ -26,15 +30,34 @@ namespace MT_CustomMaps
             {
                 GameObject target = GameObject.Find("MT_CustomMapLocation");
 
-                MusicPlayer.instance.transform.parent = target.transform;
+                lastPos = MusicPlayer.instance.transform.position;
+                lastRot = MusicPlayer.instance.transform.eulerAngles;
+                posUpdated = true;
+
+                MusicPlayer.instance.transform.SetParent(target.transform, true);
                 MusicPlayer.instance.transform.localPosition = Vector3.zero;
                 MusicPlayer.instance.transform.localEulerAngles = Vector3.zero;
+
+                Console.WriteLine("MT_Computer moved to map");
             }
-            catch
+            catch(Exception e)
             {
-                Console.WriteLine("Error moving JukeBox to custom map - MT location likely missing in map\n");
+                Console.WriteLine("MT_Computer not moved to map:\n" + e);
                 return;
             }
         }
+        /*  DOESN'T WORK AS MAP LEAVE
+        public void OnMapChange(bool enter)
+        {
+            Console.WriteLine("Map Change / Leave");
+            if (enter || !posUpdated) return;
+            MusicPlayer.instance.transform.SetParent(null, true);
+            MusicPlayer.instance.transform.position = lastPos;
+            MusicPlayer.instance.transform.eulerAngles = lastRot;
+            posUpdated = false;
+
+            Console.WriteLine("MT_Computer moved back to previous pos");
+        }
+        */
     }
 }
---
+++ src/custom maps/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MT_CustomMaps
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.frogrilla.gorillatag.mt_custommaps";
+        public const string Name = "MT_CustomMaps";
+        public const string Version = "1.0.2";
+    }
+}
